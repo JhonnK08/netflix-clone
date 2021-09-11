4c629744b4f2171bcfe7c2e62abcb638a20a9e30
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2021-09-10 23:37:32 -0300

    created Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Header.css';
+
+const Header = ({ black }) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png" alt="Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/user">
+                    <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Meu Perfil" />
+                </a>
+            </div>
+        </header>
+    );
+}
+
+
+export default Header;
